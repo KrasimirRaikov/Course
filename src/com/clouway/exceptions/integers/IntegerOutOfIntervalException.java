b954310e7f528e4aca217ce7a2d4e8e37af7e166
KrasimirRaikov <raikov.krasimir@gmail.com>
2015-08-11 15:42:54 +0300

    int exc

---
+++ src/com/clouway/exceptions/integers/IntegerOutOfIntervalException.java
@@ -0,0 +1,9 @@
+package com.clouway.exceptions.integers;
+
+/**
+ * @author raikov.krasimir@gmail.com (Krasimir Raikov)
+ */
+public class IntegerOutOfIntervalException extends Exception {
+  public IntegerOutOfIntervalException(){
+  }
+}
