42eb0d6895fedad0e5be53901698a0e97473cb10
KrasimirRaikov <raikov.krasimir@gmail.com>
2015-08-11 14:23:23 +0300

    exceptions sumator

---
+++ src/com/clouway/exceptions/sumator/Executor.java
@@ -0,0 +1,19 @@
+package com.clouway.exceptions.sumator;
+
+/**
+ * @author raikov.krasimir@gmail.com (Krasimir Raikov)
+ */
+public class Executor {
+  public static void main(String[] args){
+    Sumator sumator= new Sumator();
+
+    String aS= "What's ";
+    String bS= "up?";
+    System.out.println(sumator.sum(aS, bS));
+
+    System.out.println(sumator.sum("-43", "7545823"));
+
+
+  }
+
+}
---
+++ src/com/clouway/exceptions/sumator/Sumator.java
@@ -0,0 +1,37 @@
+package com.clouway.exceptions.sumator;
+
+
+
+/**
+ * @author raikov.krasimir@gmail.com (Krasimir Raikov)
+ */
+class Sumator {
+
+  /**
+   * Adds two Strings together
+   *
+   * @param a String one
+   * @param b String two
+   * @return returns the int that is the result of the two Strings
+   */
+  public float sum(String a, String b) {
+    try {
+      float s = Float.parseFloat(a);
+      float t = Float.parseFloat(b);
+      return s + t;
+    } catch (java.lang.NumberFormatException e) {
+      printFalse();
+      return 0.030003f;
+    }
+
+  }
+  public void printFalse(){
+
+    System.out.print("incorrect input, error №: ");
+
+  }
+
+
+
+
+}
